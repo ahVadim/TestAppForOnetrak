d99d313eb16a05b0e738bb223a7ed52fc7beaf4d
Vadim Ahtyamov <datamod93@gmail.com>
2019-01-04 18:20:28 +0300

    подготовлена разметка под recycler view list item

---
+++ app/src/main/java/com/example/vadim/testappforonetrak/DataHolder.java
@@ -0,0 +1,15 @@
+package com.example.vadim.testappforonetrak;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+public class DataHolder extends RecyclerView.ViewHolder {
+    public DataHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+
+    public void bind(){
+
+    }
+}
